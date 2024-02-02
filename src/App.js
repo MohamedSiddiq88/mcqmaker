ff447f42fd63a26fcee0cf7c530f9277e067aca2
MohamedSiddiq88 <diddiq88@gmail.com^c>
2024-02-02 11:20:22 +0530

    clean up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 
 function App() {
+  // mcq array
+  // options array inside mcq array
+  // one text area for qestion
+  // four input box for options
+  // previous and next button
+  // complete button
+  // --------advance
+  // after clickcing the complete button generate mcq with random option (HTML file)
   return (
     <div className="App">
       
